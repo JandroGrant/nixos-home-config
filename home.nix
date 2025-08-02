8cf26529dc45d7139d94898e1b986ad7a448276f
JandroGrant <jandrocottonball@gmail.com>
2025-08-02 13:35:55 -0700

    trying to make starship work

--- home.nix
+++ home.nix
@@ -108,6 +108,10 @@
   programs.starship = {
     enable = true;
     enableBashIntegration = true;
+    settings = {
+      add_newline = false;
+      format = "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
+    };
   };
 
   programs.nvf = {
