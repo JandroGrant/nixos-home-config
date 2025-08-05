21731ed882dd96ef1a852007ce38e4670c331d86
JandroGrant <jandrocottonball@gmail.com>
2025-08-05 13:47:48 -0700

    Undo

--- nvf/default.nix
+++ nvf/default.nix
@@ -1,15 +1,7 @@
 {
-  inputs,
-  pkgs,
-  ...
-}: {
   imports = [
     ./plugins.nix
     ./settings.nix
     ./mappings.nix
-    (import ./love2d-nvim.nix {
-      inherit inputs;
-      inherit pkgs;
-    })
   ];
 }
