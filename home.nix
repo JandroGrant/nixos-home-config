7dde4e4686952c85a453ed83a04aa4339503227e
JandroGrant <jandrocottonball@gmail.com>
2025-08-13 13:17:01 -0700

    modrinth didn't work, reverting to prismlauncher

--- home.nix
+++ home.nix
@@ -37,16 +37,7 @@
     spotify-player
     sameboy
     nestopia-ue
-    (modrinth-app.overrideAttrs (oldAttrs: {
-      buildCommand =
-        ''
-          gappsWrapperArgs+=(
-            --set GDK_BACKEND x11
-            --set WEBKIT_DISABLE_DMABUF_RENDERER 1
-          )
-        ''
-        + oldAttrs.buildCommand;
-    }))
+    prismlauncher
 
     # terminal tools
     yazi
