dff9b4eb4f9d82e68cc3a8c100792e35393026c1
JandroGrant <jandrocottonball@gmail.com>
2025-08-13 17:27:17 -0700

    cleaned up home.nix. also added irssi

--- home.nix
+++ home.nix
@@ -7,148 +7,159 @@
   spicetify-nix,
   ...
 }: {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "jandro";
-  home.homeDirectory = "/home/jandro";
+  home = {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    # home.username = "jandro";
+    # home.homeDirectory = "/home/jandro";
+    username = "jandro";
+    homeDirectory = "/home/jandro";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    # home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # de
-    waybar
-    hyprshot
-    hyprpaper
-    rofi
-    inputs.zen-browser.packages."${system}".default
+    stateVersion = "25.05";
 
-    # gaming
-    discord
-    r2modman
-    spotify-player
-    sameboy
-    nestopia-ue
-    prismlauncher
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      # de
+      waybar
+      hyprshot
+      hyprpaper
+      rofi
+      inputs.zen-browser.packages."${system}".default
 
-    # terminal tools
-    yazi
-    bat
-    tree
-    fastfetch
-    cmatrix
-    gtypist
-    starship
+      # gaming
+      discord
+      r2modman
+      spotify-player
+      sameboy
+      nestopia-ue
+      prismlauncher
 
-    # tools
-    vlc
-    gimp-with-plugins
+      # terminal tools
+      yazi
+      bat
+      tree
+      fastfetch
+      cmatrix
+      gtypist
+      starship
 
-    # development
-    godot
-    unzip
-    lua
-    cargo
-    rustc
-    clang
-    go
+      # tools
+      vlc
+      gimp-with-plugins
+      unzip
 
-    aseprite
-    milkytracker
-    furnace
+      # development
+      godot
+      lua
+      cargo
+      clang
+      go
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-  ];
+      aseprite
+      milkytracker
+      furnace
 
-  home.file = {
-    # HYPRLAND
-    ".local/share/icons/miku-cursor-linux/".source = ./jandro/.local/share/icons/miku-cursor-linux;
-    ".config/hypr/".source = ./jandro/.config/hypr;
-  };
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    ];
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+    home.file = {
+      # HYPRLAND
+      ".local/share/icons/miku-cursor-linux/".source = ./jandro/.local/share/icons/miku-cursor-linux;
+      ".config/hypr/".source = ./jandro/.config/hypr;
+    };
 
-  programs.ghostty = {
-    enable = true;
-    enableBashIntegration = true;
-    installVimSyntax = true;
-
-    settings = {
-      background-opacity = 0.75;
-      theme = "Oxocarbon";
+    sessionVariables = {
+      EDITOR = "nvim";
     };
   };
 
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-  };
-
-  programs.starship = {
-    enable = true;
-    enableBashIntegration = true;
-    settings = {
-      add_newline = true;
-      format = lib.concatStrings [
-        "$shlvl"
-        "$shell"
-        "$username"
-        "$hostname"
-        "$nix_shell"
-        "$git_branch"
-        "$git_commit"
-        "$git_state"
-        "$git_status"
-        "$directory"
-        "$jobs"
-        "$cmd_duration"
-        "$character"
-      ];
+  programs = {
+    bash = {
+      enable = true;
+      enableCompletion = true;
     };
-  };
 
-  programs.zoxide = {
-    enable = true;
-    enableBashIntegration = true;
-  };
-
-  programs.fzf = {
-    enable = true;
-    enableBashIntegration = true;
-  };
-
-  programs.nvf = {
-    enable = true;
-    settings = ./nvf;
-  };
-
-  programs.spicetify = {
-    enable = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "JandroGrant";
-    userEmail = "jandrocottonball@gmail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
     };
-  };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    ghostty = {
+      enable = true;
+      enableBashIntegration = true;
+      installVimSyntax = true;
+
+      settings = {
+        background-opacity = 0.75;
+        theme = "Oxocarbon";
+      };
+    };
+
+    git = {
+      enable = true;
+      userName = "JandroGrant";
+      userEmail = "jandrocottonball@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+
+    irssi = {
+      enable = true;
+    };
+
+    nvf = {
+      enable = true;
+      settings = ./nvf;
+    };
+
+    spicetify = {
+      enable = true;
+    };
+
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+      settings = {
+        add_newline = true;
+        format = lib.concatStrings [
+          "$shlvl"
+          "$shell"
+          "$username"
+          "$hostname"
+          "$nix_shell"
+          "$git_branch"
+          "$git_commit"
+          "$git_state"
+          "$git_status"
+          "$directory"
+          "$jobs"
+          "$cmd_duration"
+          "$character"
+        ];
+      };
+    };
+
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+  };
 }
